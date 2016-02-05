bb0a58bc37f2713c69fdba3d9015177967aa93ad
Nbjohnston86 <Nicko_Johnston@hotmail.com>
2016-02-05 17:09:23 -0500

    Included required Enumeration file.

---
+++ ParsingSolution/Parser.BusinessLogic/Enums/ParsableEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.BusinessLogic.Enums
+{
+    public enum ParsableEnum
+    {
+        Fusion,
+        Card,
+        Detail
+    }
+}
