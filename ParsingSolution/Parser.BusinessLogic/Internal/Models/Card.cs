6e0cd2e2cf72a37c5a29b6b00eab0a6719b92e4f
Nbjohnston86 <Nicko_Johnston@hotmail.com>
2016-02-05 19:32:36 -0500

    Added Implementation to ICard.

--- ParsingSolution/Parser.BusinessLogic/Internal/Models/Card.cs
+++ ParsingSolution/Parser.BusinessLogic/Internal/Models/Card.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parser.BusinessLogic.Internal.Interfaces;
 
 namespace Parser.BusinessLogic.Internal.Models
 {
-    class Card
+    class Card : ICard
     {
+        public int? Attack { get; set; }
+
+
+        public string Attribute
+        { get; set; }
+
+        public string CardType
+        { get; set; }
+
+        public int DeckCost
+        { get; set; }
+
+        public int? Defense
+        { get; set; }
+
+        public string Description
+        { get; set; }
+
+        public ICollection<IFusable> FusionMaterials
+        { get; set; }
+
+        public int? Level
+        { get; set; }
+
+        public ICollection<int> ModifiableByCardNumbers
+        { get; set; }
+
+        public string Name
+        { get; set; }
+
+        public int Number
+        { get; set; }
+
+        public HashSet<string> SubTypes
+        { get; set; }
     }
 }
