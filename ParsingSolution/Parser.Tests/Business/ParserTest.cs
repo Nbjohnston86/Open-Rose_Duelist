9972f9200031536c07057560c309d57b310a7da2
Nbjohnston86 <Nicko_Johnston@hotmail.com>
2016-02-08 19:04:21 -0500

    Included first test classes. Updated references to include other projects in the solution.

---
+++ ParsingSolution/Parser.Tests/Business/ParserTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Parser.BusinessLogic;
+using Parser.BusinessLogic.Enums;
+using Parser.BusinessLogic.Interfaces;
+
+namespace Parser.Tests.Business
+{
+    public class ParserTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            IRoseParser testParser = new RoseParser();
+            testParser.ConvertToJson(new);
+        }
+    }
+
+    internal class TestParsable : IRoseParsable
+    {
+        TestParsable(){
+
+            FilePath = "TestPath";
+            FileType = ParsableEnum.Card;
+        }
+
+        public string FilePath
+        {
+            get; set;
+        }
+
+        public ParsableEnum FileType
+        {
+            get; set;
+        }
+    }
+}
